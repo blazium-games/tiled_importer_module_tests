9f0dc9ffb4292ef0c9ff89a4113063ea284361db
sshiiden <niksashi42@gmail.com>
2026-06-28 13:42:44 +0200

    Update class names

--- tests/test_tileson_wrapper.gd
+++ tests/test_tileson_wrapper.gd
@@ -1,12 +1,12 @@
 extends AutoworkTest
 
 func test_tileson_parser_validity() -> void:
-    # Validate the GodotTsonTileson endpoint
-    var tson = GodotTsonTileson.new()
-    assert_not_null(tson, "Failed to instantiate GodotTsonTileson")
+    # Validate the TiledTileson endpoint
+    var tson = TiledTileson.new()
+    assert_not_null(tson, "Failed to instantiate TiledTileson")
 
     # Parse an existing json file
-    var map: GodotTsonMap = tson.parse_file("res://tests/data/test-maps/simple_map.json")
+    var map: TiledMap = tson.parse_file("res://tests/data/test-maps/simple_map.json")
     assert_not_null(map, "parse_file returned null for simple_map.json")
     assert_eq(map.get_status(), "OK", "Map parse status should be OK")
 
@@ -31,8 +31,8 @@ func test_tileson_parser_validity() -> void:
     var layers: Array = map.get_layers()
     assert_true(layers.size() > 0, "Map should contain at least one layer")
     
-    var first_layer: GodotTsonLayer = layers[0]
-    assert_not_null(first_layer, "Layer should be properly cast to GodotTsonLayer")
+    var first_layer: TiledLayer = layers[0]
+    assert_not_null(first_layer, "Layer should be properly cast to TiledLayer")
     assert_true(first_layer.get_name() != "", "Layer should have a name")
     assert_true(first_layer.get_type() != "", "Layer should have a type")
     
@@ -47,7 +47,7 @@ func test_tileson_parser_validity() -> void:
     # Validate Tilesets
     var tilesets: Array = map.get_tilesets()
     assert_true(tilesets.size() > 0, "Map should contain standard tilesets")
-    var first_ts: GodotTsonTileset = tilesets[0]
+    var first_ts: TiledTileset = tilesets[0]
     assert_true(first_ts.get_name() != "", "Tileset should have a name")
     
     # Tileset newly bound properties check
@@ -64,8 +64,8 @@ func test_tileson_parser_validity() -> void:
     assert_true(props is Array, "get_properties should return an Array")
 
 func test_tileson_objects_and_chunks() -> void:
-    var tson = GodotTsonTileson.new()
-    var map: GodotTsonMap = tson.parse_file("res://tests/data/test-maps/simple_map.json")
+    var tson = TiledTileson.new()
+    var map: TiledMap = tson.parse_file("res://tests/data/test-maps/simple_map.json")
     
     for layer in map.get_layers():
         var objects = layer.get_objects()
@@ -75,7 +75,7 @@ func test_tileson_objects_and_chunks() -> void:
         assert_true(chunks is Array, "get_chunks should return an Array")
         
         for obj in objects:
-            assert_not_null(obj, "GodotTsonObject should be properly formed")
+            assert_not_null(obj, "TiledObject should be properly formed")
             var obj_id = obj.get_id()
             var obj_name = obj.get_name()
             # Assert execution context is safe
@@ -88,28 +88,28 @@ func test_tileson_objects_and_chunks() -> void:
             assert_true(typeof(obj.get_polyline()) == TYPE_ARRAY, "polyline should export to array")
         
         for chunk in chunks:
-            assert_not_null(chunk, "GodotTsonChunk should be properly formed")
+            assert_not_null(chunk, "TiledChunk should be properly formed")
             assert_true(chunk.get_data() is Array, "Chunk data should export to array")
 
 func test_tileson_project_wrapper() -> void:
-    var project = GodotTsonProject.new()
+    var project = TiledProject.new()
     assert_not_null(project, "Project Wrapper successfully bound")
     assert_eq(project.get_path(), "", "Project should start with empty path natively")
     
-    var world = GodotTsonWorld.new()
+    var world = TiledWorld.new()
     assert_not_null(world, "World Wrapper successfully bound")
 
 func test_tileson_wang_wrapper() -> void:
-    var wang = GodotTsonWangSet.new()
+    var wang = TiledWangSet.new()
     assert_not_null(wang, "WangSet successfully bound natively")
     assert_eq(wang.get_name(), "", "Wang should instantiate safely")
     assert_true(typeof(wang.get_class_type()) == TYPE_STRING, "WangSet class type should be string")
     
-    var wcolor = GodotTsonWangColor.new()
+    var wcolor = TiledWangColor.new()
     assert_not_null(wcolor, "WangColor successfully bound")
     assert_true(typeof(wcolor.get_class_type()) == TYPE_STRING, "WangColor class type should be string")
     
-    var wtile = GodotTsonWangTile.new()
+    var wtile = TiledWangTile.new()
     assert_not_null(wtile, "WangTile successfully bound")
     assert_true(typeof(wtile.has_d_flip()) == TYPE_BOOL, "WangTile d_flip should be bool")
     assert_true(typeof(wtile.has_h_flip()) == TYPE_BOOL, "WangTile h_flip should be bool")
@@ -117,39 +117,39 @@ func test_tileson_wang_wrapper() -> void:
     assert_true(typeof(wtile.get_tile_id()) == TYPE_INT, "WangTile id should be integer")
 
 func test_tileson_remaining_classes() -> void:
-    var grid := GodotTsonGrid.new()
-    assert_not_null(grid, "GodotTsonGrid bound")
-    var terrain := GodotTsonTerrain.new()
-    assert_not_null(terrain, "GodotTsonTerrain bound")
-    var text := GodotTsonText.new()
-    assert_not_null(text, "GodotTsonText bound")
+    var grid := TiledGrid.new()
+    assert_not_null(grid, "TiledGrid bound")
+    var terrain := TiledTerrain.new()
+    assert_not_null(terrain, "TiledTerrain bound")
+    var text := TiledText.new()
+    assert_not_null(text, "TiledText bound")
     assert_true(typeof(text.get_horizontal_alignment()) == TYPE_INT, "Text horiz align should be int")
     assert_true(typeof(text.get_vertical_alignment()) == TYPE_INT, "Text vert align should be int")
     
-    var transf := GodotTsonTransformations.new()
-    assert_not_null(transf, "GodotTsonTransformations bound")
+    var transf := TiledTransformations.new()
+    assert_not_null(transf, "TiledTransformations bound")
     assert_true(typeof(transf.allow_hflip()) == TYPE_BOOL, "Transformations hflip should be bool")
     assert_true(typeof(transf.allow_vflip()) == TYPE_BOOL, "Transformations vflip should be bool")
     assert_true(typeof(transf.allow_preferuntransformed()) == TYPE_BOOL, "Transformations preferuntransf should be bool")
     assert_true(typeof(transf.allow_rotation()) == TYPE_BOOL, "Transformations rotation should be bool")
-    var tclass := GodotTsonTiledClass.new()
-    assert_not_null(tclass, "GodotTsonTiledClass bound")
-    var enumdef := GodotTsonEnumDefinition.new()
-    assert_not_null(enumdef, "GodotTsonEnumDefinition bound")
+    var tclass := TiledClass.new()
+    assert_not_null(tclass, "TiledClass bound")
+    var enumdef := TiledEnumDefinition.new()
+    assert_not_null(enumdef, "TiledEnumDefinition bound")
     assert_true(typeof(enumdef.get_storage_type()) == TYPE_INT, "EnumDef storage type should be integer")
     assert_true(typeof(enumdef.has_value("string")) == TYPE_BOOL, "EnumDef has_value should exist")
     assert_true(typeof(enumdef.has_value_id(1)) == TYPE_BOOL, "EnumDef has_value_id should exist")
     
-    var enumval := GodotTsonEnumValue.new()
-    assert_not_null(enumval, "GodotTsonEnumValue bound")
+    var enumval := TiledEnumValue.new()
+    assert_not_null(enumval, "TiledEnumValue bound")
     assert_true(typeof(enumval.contains_value_name("name")) == TYPE_BOOL, "EnumValue contains_value_name exists")
-    var tileobj := GodotTsonTileObject.new()
-    assert_not_null(tileobj, "GodotTsonTileObject bound")
-    var ptypes := GodotTsonProjectPropertyTypes.new()
-    assert_not_null(ptypes, "GodotTsonProjectPropertyTypes bound")
+    var tileobj := TiledTileObject.new()
+    assert_not_null(tileobj, "TiledTileObject bound")
+    var ptypes := TiledProjectPropertyTypes.new()
+    assert_not_null(ptypes, "TiledProjectPropertyTypes bound")
 func test_exhaustive_method_calls() -> void:
 
-    var class_names = ['GodotTsonTileson', 'GodotTsonLayer', 'GodotTsonTileset', 'GodotTsonWangColor', 'GodotTsonWangTile', 'GodotTsonWangSet', 'GodotTsonFrame', 'GodotTsonAnimation', 'GodotTsonTile', 'GodotTsonObject', 'GodotTsonChunk', 'GodotTsonMap', 'GodotTsonProperty', 'GodotTsonProjectFolder', 'GodotTsonProjectData', 'GodotTsonProject', 'GodotTsonWorldMapData', 'GodotTsonWorld', 'GodotTsonGrid', 'GodotTsonTerrain', 'GodotTsonText', 'GodotTsonTransformations', 'GodotTsonTiledClass', 'GodotTsonEnumDefinition', 'GodotTsonEnumValue', 'GodotTsonTileObject', 'GodotTsonProjectPropertyTypes']
+    var class_names = ['TiledTileson', 'TiledLayer', 'TiledTileset', 'TiledWangColor', 'TiledWangTile', 'TiledWangSet', 'TiledFrame', 'TiledAnimation', 'TiledTile', 'TiledObject', 'TiledChunk', 'TiledMap', 'TiledProperty', 'TiledProjectFolder', 'TiledProjectData', 'TiledProject', 'TiledWorldMapData', 'TiledWorld', 'TiledGrid', 'TiledTerrain', 'TiledText', 'TiledTransformations', 'TiledClass', 'TiledEnumDefinition', 'TiledEnumValue', 'TiledTileObject', 'TiledProjectPropertyTypes']
 
     var total_called = 0
 
